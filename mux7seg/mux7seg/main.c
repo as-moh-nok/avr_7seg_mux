64badbb9f6a5bcf282677e0a8cc0ae2bed7839b7
AS-MOH-NOK <a.mohamma.nok@gmail.com>
2026-02-03 12:18:25 +0330

    Show 1405 in 4 ssegment as prototype

--- mux7seg/mux7seg/main.c
+++ mux7seg/mux7seg/main.c
@@ -8,19 +8,20 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "mux7seg.c"
-#define F_CPU 16000000UL // 16 MHz clock speed
+#define F_CPU 8000000UL // 16 MHz clock speed
 
 int main(void)
 {
 	//SET OUTPUT
 	DDRC = 0xFF;
-	//PORTC = 0x00;
+	PORTC = 0x06;
 	
 	DDRA = 0xFF;
+	PORTA = 0x01;
 	
-	/*
+	
 	_delay_ms(2000);	
-	segment_init(2,1);
+	/*segment_init(2,1);
 	segment_putchar('3');
 //	segment_full();//
 	_delay_ms(2000);	
@@ -30,20 +31,21 @@ int main(void)
     while (1) 
     {
 		PORTC = 0x79;
-		PORTA = 0x01;
-		_delay_ms(500);
-		
-		PORTC = 0x19;
-		PORTA = 0x02;
-		_delay_ms(500);
-		
-		PORTC = 0x04;
-		PORTA = 0x04;
-		_delay_ms(500);
-		
-		PORTC = 0x19;
 		PORTA = 0x08;
-		_delay_ms(500);
+		_delay_ms(50);
+		
+		PORTC = 0x19;
+		PORTA = 0x04;
+		_delay_ms(50);
+	
+		PORTC = 0x40;
+		PORTA = 0x02;
+		_delay_ms(50);
+		
+		
+		PORTC = 0x12;
+		PORTA = 0x01;
+		_delay_ms(50);
 
 		/*
 		for(unsigned int i = 1;i<4;i++)
